2f7e9f4e39a284fc7cd0f3e04b8cf35fa139a836
assavinda <vic12600@gmail.com>
2025-02-12 04:25:16 +0700

    predownload images

--- components/scene/14.vue
+++ components/scene/14.vue
@@ -271,7 +271,7 @@ function nextgame() {
     curtain.value.classList.remove('hidden')
     function step() {
         if (c >= 0) {
-            if (props.images.value.get('curtain' + c)) {
+            if (props.images.value.has('curtain' + c)) {
                 curtain.value.src = props.images.value.get('curtain' + c)
             }
             c--
